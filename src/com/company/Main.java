42aa47c561ac274cb74c38b6a5f347288e7976ba
Xenorus <ais191telegina@ya.ru>
2021-09-22 12:38:34 +0500

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,48 @@
 package com.company;
-
 import java.util.Scanner;
 
 public class Main {
+
+    public static String alphabet_RU = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя";
+    public static String alphabet_EN = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите текст: ");
         String message = scanner.nextLine();
-        System.out.println("Введите шаг дешифровки: ");
+        System.out.println("Введите шаг шифровки: ");
         int step = scanner.nextInt();
-        System.out.println("Введите язык (EN или RU: ");
+        System.out.println("Введите язык (EN или RU): ");
         String lang = scanner.next();
+        String scr;
+        boolean a = true;
+        while (a) {
+            System.out.println("Введите 1, если хотите зашифровать сообщение, или 2, если хотите дешифровать: ");
+            scr = scanner.next();
+            if (scr.equals("1")) {
+                System.out.println("Результат шифровки: " + Caesar(message, step, lang));
+                a = false;
+                break;
+            }
+            if (scr.equals("2")) {
+                System.out.println("Результат дешифровки: " + decryptCaesar(message, step, lang));
+                a = false;
+                break;
+            }
+            System.out.println("Введено неверное значение, попробуйте ввести 1 или 2 снова.");
+        }
+    }
 
-        System.out.println("Результат шифровки: " + Caesar(message, step, lang));
+    public static String decryptCaesar(String mssg, int step, String lang) {
+        step = -1*step;
+        return Caesar(mssg, step, lang);
     }
 
     public static String Caesar(String mssg, int step, String lang) {
-        String alphabet_RU = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя";
-        String alphabet_EN = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
         String result = "";
         char symbol_mssg;
-        char symbol_found;
+        char symbol_found = 'a';
         int number;
         int index;
 
